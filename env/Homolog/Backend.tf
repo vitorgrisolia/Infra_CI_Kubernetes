b0a757e35595503d9ea72179190586362e2a9076
Vitor Grisolia <grisolia.vitor@gmail.com>
2024-09-06 15:56:09 -0300

    bucket

--- env/Homolog/Backend.tf
+++ env/Homolog/Backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-state-vito-r859cnshjp9bjoyfjntbie8jcgwuquse2b-s3alias"
-    key    = "Prod/terraform.tfstate"
+    key    = "env:/terraform.tfstate"
     region = "us-west-2"
   }
 }
