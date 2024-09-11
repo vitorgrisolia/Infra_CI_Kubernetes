85c30d60ec54437fb58ef825caa0787f6d2db283
Vitor Grisolia <grisolia.vitor@gmail.com>
2024-09-11 09:52:43 -0300

    ajustes de bug

--- infra/Provider.tf
+++ infra/Provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.47"
+      version = "~> 3.27"
     }
   }
 
@@ -10,31 +10,5 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-east-2"
-}
-
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
-    command     = "aws"
-  }
-}
-
-resource "kubernetes_service" "LoadBalancer" {
-  metadata {
-    name = "load-balancer-go-api"
-  }
-  spec {
-    selector = {
-      nome = "go"
-    }
-    port {
-      port = 8000
-      target_port = 8000
-    }
-    type = "LoadBalancer"
-  }
+  region  = "us-west-2"
 }
